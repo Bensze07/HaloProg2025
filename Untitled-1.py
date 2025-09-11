548461169a6329333af5920868ffc6315f09c4d9
Bensze07 <bencetikkszita@gmail.com>
2025-09-11 09:09:18 +0200

    verzio 1

---
+++ Untitled-1.py
@@ -0,0 +1,30 @@
+
+# Lista feltöltése 100 db random kétjegyű egész számokkal
+for i in range(100):
+    szam=random.randint(10,99)
+    szamok.append(szam)
+    
+#Ellenőrzés
+print(szamok)
+
+#EGYSZÁM JÁTÉK
+
+jatek_szam = 0
+nem_talaldDB = 0
+
+kitalalando_szam = szamok[random.randint(len(szamok))]
+
+tipp = int(input("Tipped?: (egész szám): "))
+
+while(tipp != kitalalando_szam):
+    tipp = int(input("Tipped?: (egész szám): "))
+   
+print("Kitalatad a kitalalando szamt!")
+
+folytatás = input("Akarsz-e még jatszani? [I/N]")
+
+if(folytatás == "I"):
+    #??????
+else:
+    exit()
+
